4535e5ddaab4badc23b60b1aaf85162b0c36605d
Hardik Kanjariya <myhardy8520112@gmail.com>
2018-08-27 00:02:50 +0530

    AgedBrieItem and BackstagePassesItem Modules Added & Remove Redundant Tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,6 +3,8 @@ require_relative '../lib/item_factory'
 require_relative '../lib/inventory_item'
 require_relative '../lib/conjured_item'
 require_relative '../lib/sulfuras_item'
+require_relative '../lib/aged_brie_item'
+require_relative '../lib/backstage_passes_item'
 
 describe 'GildedRose' do
   subject { GildedRose.new }
@@ -11,125 +13,46 @@ describe 'GildedRose' do
   it { should respond_to(:items) }
 
   context "update_quality was called" do
-      let( :vanilla_item ) { subject.items[0] }
-      let( :aged_brie ) { subject.items[1] }
-      let( :sulfuras ) { subject.items[3] }
-      let( :backstage_passes ) { subject.items[4] }
-      let( :conjured ) { subject.items[5] }
+    let( :vanilla_item ) { subject.items[0] }
+    let( :aged_brie ) { subject.items[1] }
+    let( :sulfuras ) { subject.items[3] }
+    let( :backstage_passes ) { subject.items[4] }
+    let( :conjured ) { subject.items[5] }
 
-      context "base item behavior" do
+    context "base item behavior" do
 
-        it "should decrease sell_in by 1" do
+      it "should decrease sell_in by 1" do
+        expect {
+          subject.update_quality
+        }.to change{ vanilla_item.sell_in }.by(-1)
+      end
+
+      it "should decrease quality by 1" do
+        expect {
+          subject.update_quality
+        }.to change{ vanilla_item.quality }.by(-1)
+      end
+
+      it "should always have a non-negative quality" do
+        expect {
+          (vanilla_item.sell_in * 10).times {subject.update_quality}
+        }.to change{ vanilla_item.quality }.to(0)
+      end
+
+      context "when sell_in expires" do
+        before(:each) { 10.times{ subject.update_quality } }
+
+        it "should have a sell_in < 1" do
+          expect(vanilla_item.sell_in).to be < 1
+        end
+
+        it "should decrease quality by 2 after when sell_in < 1" do
           expect {
             subject.update_quality
-          }.to change{ vanilla_item.sell_in }.by(-1)
-        end
-
-        it "should decrease quality by 1" do
-          expect {
-            subject.update_quality
-          }.to change{ vanilla_item.quality }.by(-1)
-        end
-
-        it "should always have a non-negative quality" do
-          expect {
-            (vanilla_item.sell_in * 10).times {subject.update_quality}
-          }.to change{ vanilla_item.quality }.to(0)
-        end
-
-        context "when sell_in expires" do
-          before(:each) { 10.times{ subject.update_quality } }
-
-          it "should have a sell_in < 1" do
-            expect(vanilla_item.sell_in).to be < 1
-          end
-
-          it "should decrease quality by 2 after when sell_in < 1" do
-            expect {
-              subject.update_quality
-            }.to change{ vanilla_item.quality }.by(-2)
-          end
-
-        end
-
-      end # context base item
-
-      context "Aged Brie" do
-        let!( :initial_quality ) { aged_brie.quality }
-
-        [1,10,100,1000].each do |age|
-          context "when age increases by #{age}" do
-            let!( :initial_quality ) { aged_brie.quality }
-            before(:each) { age.times{ subject.update_quality } }
-
-            it "should increase in quality" do
-              expect( aged_brie.quality).to be > initial_quality
-            end
-
-            it "should not increase in quality over 50" do
-              expect( aged_brie.quality).to be <= 50
-            end
-
-          end
-        end
-
-      end # context agied brie
-
-      context "Sulfuras" do
-        it "should never age (because it is legendary, of course)" do
-          [1,10,100].each do |age|
-            expect { subject.update_quality }.to_not change{ sulfuras.sell_in }
-          end
+          }.to change{ vanilla_item.quality }.by(-2)
         end
       end
-
-      context "Backstage passes" do
-        context "sell_in > 10" do
-          it "should have a sell_in > 10" do
-              expect( backstage_passes.sell_in ).to be > 10
-          end
-
-          it "should increase quality by 1 on update_quality" do
-            expect{ subject.update_quality }.to change{backstage_passes.quality}.by(1)
-          end
-        end
-        context "sell_in <= 10" do
-          before(:each) { 5.times{ subject.update_quality } }
-          it "should have a sell_in <= 10" do
-              expect( backstage_passes.sell_in ).to be <= 10
-          end
-
-          it "should increase quality by 2 on update_quality" do
-            expect{ subject.update_quality }.to change{backstage_passes.quality}.by(2)
-          end
-        end
-        context "sell_in <= 5" do
-          before(:each) { 10.times{ subject.update_quality } }
-          it "should have a sell_in <= 5" do
-              expect( backstage_passes.sell_in ).to be <= 5
-          end
-
-          it "should increase quality by 3 on update_quality" do
-            expect{ subject.update_quality }.to change{backstage_passes.quality}.by(3)
-          end
-        end
-        context "sell_in == 0" do
-          before(:each) { 15.times{ subject.update_quality } }
-          it "should have a sell_in == 0" do
-              expect( backstage_passes.sell_in ).to eq(0)
-          end
-
-          it "should have a quality of 0" do
-            expect{ subject.update_quality }.to change{backstage_passes.quality}.to(0)
-          end
-        end
-      end
-
-      context "Conjured" do
-        it "should reduce quality by 2 on update_quality" do
-          expect{ subject.update_quality }.to change{conjured.quality}.by(-2)
-        end
-      end
+    end
   end
 end
 
@@ -156,6 +79,14 @@ describe 'ItemFactory' do
   it "creates sulfuras items" do
     expect(subject.create "Sulfuras, Hand of Ragnaros", 0, 80).to be_kind_of(SulfurasItem)
   end
+
+  it "creates aged brie items" do
+    expect(subject.create "Aged Brie", 2, 0).to be_kind_of(AgedBrieItem)
+  end
+
+  it "created backstage passes items" do
+    expect(subject.create "Backstage passes to a TAFKAL80ETC concert", 0, 80).to be_kind_of(BackstagePassesItem)
+  end
 end
 
 describe 'InventoryItem' do
@@ -176,13 +107,79 @@ describe 'ConjuredItem' do
   
   it "should reduce quality by 2 on update_quality" do
     expect{ subject.update }.to change{subject.quality}.by(-2)
+  end  
+end
+
+describe 'SulfurasItem' do
+  subject { SulfurasItem.new :ignored, 10, 10 }
+  
+  it "should not touch quality on update_quality" do
+    expect{ subject.update }.to_not change{subject.quality}
+  end
+end
+
+describe 'AgedBrieItem' do
+  subject { AgedBrieItem.new :ignored, 10, 10}
+  let!( :initial_quality ) { subject.quality }
+    
+  [1,10,100,1000].each do |age|
+    context "when age increases by #{age}" do
+      let!( :initial_quality ) { subject.quality }      
+      before(:each) { age.times{ subject.update_quality } }
+
+      it "should increase in quality" do
+        expect( subject.quality).to be > initial_quality
+      end
+
+      it "should not increase in quality over 50" do
+        expect( subject.quality).to be <= 50
+      end
+    end
+  end
+end
+
+describe 'BackstagePassesItem' do
+  subject { BackstagePassesItem.new :ignored, 15, 20 }
+  
+  context "sell_in > 10" do
+    it "should have a sell_in > 10" do
+      expect( subject.sell_in ).to be > 10
+    end
+    
+    it "should increase quality by 1 on update" do
+      expect{ subject.update }.to change{subject.quality}.by(1)
+    end
   end
 
-  describe 'SulfurasItem' do
-    subject { SulfurasItem.new :ignored, 10, 10 }
+  context "sell_in <= 10" do
+    before(:each) { 5.times{ subject.update } }
+    it "should have a sell_in <= 10" do
+      expect( subject.sell_in ).to be <= 10
+    end
     
-    it "should not touch quality on update_quality" do
-      expect{ subject.update }.to_not change{subject.quality}
+    it "should increase quality by 2 on update" do
+      expect{ subject.update }.to change{subject.quality}.by(2)
+    end
+  end
+
+  context "sell_in <= 5" do
+    before(:each) { 10.times{ subject.update } }
+    it "should have a sell_in <= 5" do
+      expect( subject.sell_in ).to be <= 5
+    end
+    
+    it "should increase quality by 3 on update" do
+      expect{ subject.update }.to change{subject.quality}.by(3)
+    end
+  end
+  context "sell_in == 0" do
+    before(:each) { 14.times{ subject.update } }
+    it "should have a sell_in == 0" do
+      expect{ subject.update }.to change{subject.sell_in}.to(0)
+    end
+    
+    it "should have a quality of 0" do
+      expect{ subject.update }.to change{subject.quality}.to(0)
     end
   end
 end
